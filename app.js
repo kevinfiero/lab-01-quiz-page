b682dea5f31d8aeeb15eb0bd202c56b4d840f4dc
kevinfiero <kjf46@cornell.edu>
2020-09-28 17:36:53 -0700

    added bulleted lists and formatting

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-import { translateToAYes } from './translate-to-a-yes.js';
+import { countsAsAYes } from './counts-as-a-yes.js';
 
 const quizButton = document.getElementById('quiz-button');
 const correctAnswers = document.getElementById('correct-answers');
 
 quizButton.addEventListener('click', () => {
-    const nameVar = prompt('What is your name?');
+    let nameVar = prompt('What is your name?');
+
+    if (nameVar === ''){
+        nameVar = 'User';
+    }
 
     if (nameVar === null){
         return;
@@ -18,19 +22,19 @@ quizButton.addEventListener('click', () => {
 
     const bulbasaurQuestion = prompt('Is it true that Bulbasaur is blue-green in color?');
     
-    if (translateToAYes(bulbasaurQuestion)){
+    if (countsAsAYes(bulbasaurQuestion)){
         counter++;
     }
 
     const charmanderQuestion = prompt('Is it true that Charmander is a quadruped?');
 
-    if (!translateToAYes(charmanderQuestion)){
+    if (!countsAsAYes(charmanderQuestion)){
         counter++;
     }
 
     const squirtleQuestion = prompt('Is it true that Squirtle has a curly tail?');
 
-    if (translateToAYes(squirtleQuestion)){
+    if (countsAsAYes(squirtleQuestion)){
         counter++;
     }
 
@@ -38,19 +42,26 @@ quizButton.addEventListener('click', () => {
     
     let resultString;
     let resultPercent;
+    let backgroundColor;
 
     resultPercent = Math.round((counter / 3) * 100);
 
     if (counter === 3){
         resultString = `Congratulations, ${nameVar}! You answered ${resultPercent}% of the questions correctly!`;
+        backgroundColor = '#277da1';
     } else if (counter === 2){
         resultString = `${nameVar}, you answered ${resultPercent}% of the questions correctly. Only one question wrong!`;
+        backgroundColor = '#2a9d8f';
     } else if (counter === 1){
         resultString = `${nameVar}, you only answered ${resultPercent}% of the questions correctly. You might want to brush up a bit!`;
+        backgroundColor = '#f4a261';
     } else {
         resultString = `Unfortunately ${nameVar}, you answered ${resultPercent}% of the questions correctly. You might want to study a bit more!`;
+        backgroundColor = '#e76f51';
     }
     
+    correctAnswers.style.visibility = 'visible';
     correctAnswers.textContent = resultString;
+    correctAnswers.style.backgroundColor = backgroundColor;
 
 });
--- test/counts-as-a-yes.test.js
+++ test/counts-as-a-yes.test.js
@@ -1,11 +1,11 @@
 // IMPORT MODULES under test here:
-import { translateToAYes } from '../translate-to-a-yes.js';
+import { countsAsAYes } from '../counts-as-a-yes.js';
 
 const test = QUnit.test;
 
 test('test isYes function yields true', (expect) => {
 
-    const result = translateToAYes('Yepper');
+    const result = countsAsAYes('Yepper');
     const expectedVal = true;
 
     expect.equal(result, expectedVal);
@@ -13,7 +13,7 @@ test('test isYes function yields true', (expect) => {
 
 test('test isYes function yields false', (expect) => {
 
-    const result = translateToAYes('No ma\'am');
+    const result = countsAsAYes('No ma\'am');
     const expectedVal = false;
 
     expect.equal(result, expectedVal);
